1a24e6647f3dba36ed39426db902d7f88688c7b5
IvaTeca <ivateca@IvaTecas-MacBook-Pro.local>
2023-02-22 19:04:23 +0100

    Update pir menu-items to navigate with Router

--- ecommerce-react/src/App.js
+++ ecommerce-react/src/App.js
@@ -1,11 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { Routes,Route } from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats page</h1>
+  </div>
+)
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
+    <div >
+      <Routes>
+        <Route exact path="/" element={ <HomePage />} />
+        <Route exact path="/hats" element={ <HatsPage />} />
+      </Routes>
+        
     </div>
   );
 }
--- ecommerce-react/src/components/menu-item/menu-item.component.jsx
+++ ecommerce-react/src/components/menu-item/menu-item.component.jsx
@@ -1,17 +1,48 @@
+// import React from "react";
+// import './menu-item.styles.scss'
+// import { useHistory }  from 'react-router-dom';
+
+// const MenuItem = ({title,imageUrl,size,history,linkUrl,match}) => ( // ovo {title} je isto sto i props.title
+//         <div className={`${size} menu-item`}
+//             onClick={() => history.push(`${match.url}${linkUrl}`)}
+//             >
+//             <div className='background-image' style={{
+//                 backgroundImage: `url(${imageUrl})`
+//             }} />
+//             <div className='content'>
+//                 <div className='title'>{title.toUpperCase()}</div>
+//                 <span className='subtitle'>SHOP NOW</span>
+//             </div>
+//         </div>
+// )
+
+// export default MenuItem;
+
+
 import React from "react";
 import './menu-item.styles.scss'
+import { useNavigate } from 'react-router-dom';
 
-const MenuItem = ({title,imageUrl,size}) => ( // ovo {title} je isto sto i props.title
-        <div className={`${size} menu-item`}
-            >
-            <div className='background-image' style={{
-                backgroundImage: `url(${imageUrl})`
-            }} />
-            <div className='content'>
-                <div className='title'>{title.toUpperCase()}</div>
-                <span className='subtitle'>SHOP NOW</span>
-            </div>
-        </div>
-)
+const MenuItem = ({title, imageUrl, size, linkUrl}) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(`${linkUrl}`);
+  };
+
+  return (
+    <div className={`${size} menu-item`} onClick={handleClick}>
+      <div className='background-image' style={{backgroundImage: `url(${imageUrl})`}} />
+      <div className='content'>
+        <div className='title'>{title.toUpperCase()}</div>
+        <span className='subtitle'>SHOP NOW</span>
+      </div>
+    </div>
+  );
+}
 
 export default MenuItem;
+
+
+
+
