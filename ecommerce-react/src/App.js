f5f2f4f7e7e94fcaa37146ff1d718517bbadd8c7
IvaTeca <ivateca@IvaTecas-MacBook-Pro.local>
2023-02-22 20:25:11 +0100

    Added header component and links

--- ecommerce-react/src/App.js
+++ ecommerce-react/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 import { Routes,Route } from 'react-router-dom';
 
@@ -9,6 +10,7 @@ import { Routes,Route } from 'react-router-dom';
 function App() {
   return (
     <div >
+      <Header />
       <Routes>
         <Route exact path="/" element={ <HomePage />} />
         <Route exact path="/shop" element={ <ShopPage />} />
