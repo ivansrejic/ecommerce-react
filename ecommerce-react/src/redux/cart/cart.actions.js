2374829fd1c4f7e4768f6cceda3c6cd28a880fad
IvaTeca <ivateca@IvaTecas-MacBook-Pro.local>
2023-03-07 14:33:42 +0100

    Updated shop-data and added cart utils to group cart items

--- ecommerce-react/src/redux/cart/cart.actions.js
+++ ecommerce-react/src/redux/cart/cart.actions.js
@@ -4,3 +4,7 @@ export const toggleCartHidden = () => ({
     type: CartActionTypes.TOGGLE_CART_HIDDEN
 }) // Ovde nam ne treba payload, zato sto u reducer radimo !hidden function 
 
+export const addItem = (item) => ({
+    type: CartActionTypes.ADD_ITEM,
+    payload: item 
+})
