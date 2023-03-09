2999542370355f84f7f192e179971ce1b06e9b1e
IvaTeca <ivateca@IvaTecas-MacBook-Pro.local>
2023-03-09 20:22:32 +0100

    Added persistro

--- ecommerce-react/src/redux/root-reducer.js
+++ ecommerce-react/src/redux/root-reducer.js
@@ -1,9 +1,21 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // local storage object
+//import sessionStorage from "redux-persist/es/storage/session"; // session storage object
+
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']
+}
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer
 });
+
+export default persistReducer(persistConfig,rootReducer)
--- ecommerce-react/src/redux/store.js
+++ ecommerce-react/src/redux/store.js
@@ -1,13 +1,18 @@
 import {createStore, applyMiddleware} from 'redux';
 import logger from 'redux-logger';
+import { persistStore } from 'redux-persist';
 
 import rootReducer from './root-reducer';
 
 const middlewares = [logger]; // middleware logger, samo logguje sta se nalazi, odnosno sta je izmedju action i root-reducera
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares)); // umseto ...midlewares , moze i logger samo 
+export const store = createStore(rootReducer, applyMiddleware(...middlewares)); // umseto ...midlewares , moze i logger samo 
 
-export default store;
+export const persistor = persistStore(store);
+
+export default {store, persistor};
+
+
 
 
 
